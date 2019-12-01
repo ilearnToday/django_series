aaff52cade1ac45e459d9a5e0bade8c16b53e248
ilearnToday <goodeitime@gmail.com>
2019-12-01 15:24:58 +0200

    Start delete old images on profile change and profile deletion from media folder

--- users/models.py
+++ users/models.py
@@ -1,7 +1,7 @@
 from django.db import models
-
 from django.contrib.auth.models import User
-
+from django.dispatch import receiver
+import os
 from PIL import Image
 
 
@@ -21,3 +21,27 @@ class Profile(models.Model):
             output_size = (300, 300)
             img.thumbnail(output_size)
             img.save(self.profile_image.path)
+
+
+@receiver(models.signals.post_delete, sender=Profile)
+def auto_delete_profile_img_on_delete(sender, instance, **kwargs):
+    if instance.profile_image and instance.profile_image.name != 'default.jpg':
+        if os.path.isfile(instance.profile_image.path):
+            os.remove(instance.profile_image.path)
+
+
+@receiver(models.signals.pre_save, sender=Profile)
+def auto_delete_old_profile_img_on_update(sender, instance, **kwargs):
+    if not instance.pk:
+        return False
+    try:
+        old_img = sender.objects.get(pk=instance.pk).profile_image
+        if old_img.name == 'default.jpg':
+            return False
+    except sender.DoesNotExist:
+        return False
+    new_img = instance.profile_image
+    if not old_img == new_img:
+        if os.path.isfile(old_img.path):
+            os.remove(old_img.path)
+
